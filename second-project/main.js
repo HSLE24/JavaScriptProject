7c0e14988b5aabb4ed5706f30811f1f5a58d2d83
HSLE24 <sh911124@gmail.com>
2024-02-09 14:13:39 +0900

    second proejct

---
+++ second-project/main.js
@@ -0,0 +1,141 @@
+//유저가 값을 입력한다.
+//+ 버튼을 클릭하면, 할일이 추가된다.
+//delete 버튼을 누르면 할일이 삭제된다.
+//check 버튼을 누르면 할일이 끝나면서 밑줄이 생긴다.
+//1. check 버튼을 클릭하는 순간 true -> false
+//2. true이면 끝난 걸로 간주하고 밑줄 보여주기
+//3. false이면 안끝난 걸로 간주하고 그대로
+//진행 중, 끝남 탭을 누르면 언더바가 이동한다.
+//끝남 탭은 끝난 아이템만
+//진행 중 탭은 진행중인 아이템만
+//전체 탭을 누르면 다시 전체 아이템으로 돌아옴
+
+let taskList = []
+let taskStatus = "ALL"
+
+let taskInput = document.getElementById("task-input")
+let addButton = document.getElementById("add-button")
+let underLine = document.getElementById("under-line")
+let progressBar = document.getElementById("p-bar")
+let percentInfo = document.getElementById("percent-info")
+
+addButton.addEventListener("click", addTask)
+document.addEventListener("keypress", handleEnterKeyPress)
+
+function randomIDGenerate(){
+    return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase()
+}
+
+function addTask(){
+    let taskContent = taskInput.value
+
+    if (taskInput.value == ""){
+        return;
+    }
+
+    let task = {
+        id: randomIDGenerate(),
+        taskContent : taskInput.value,
+        isComplete: false
+    }
+
+    taskList.push(task)
+    render(taskStatus)
+
+    taskInput.value = ""
+}
+
+function handleEnterKeyPress(event){
+    if (event.which === 13 || event.keyCode === 13){
+        if (!addButton.disabled){
+            addTask()
+        }
+    }
+}
+
+function render(status){
+    let completeCount = 0
+    let resultHTML = ""
+
+    for (let i = 0; i < taskList.length; i++){
+        if ((taskList[i].isComplete == true)){
+            
+            if (status == "ALL"|| status == "END"){
+                resultHTML += `
+                <div  class="task task-area-done">
+                    <div class="task-done">                    
+                        ${taskList[i].taskContent}
+                    </div>
+                    <div>
+                        <button onclick="toggleComplete('${taskList[i].id}')" class="btn btn-light"><i class="fa-solid fa-play"></i></button>
+                        <button onclick="deleteTask('${taskList[i].id}')" id="delete-button" class="btn btn-light"><i class="fa-solid fa-stop"></i></button>
+                    </div>
+                </div>
+                `
+            }
+
+            completeCount++
+        }
+        else if ((taskList[i].isComplete != true) && (status == "ALL"|| status == "START")) {
+            resultHTML += `
+            <div class="task">
+                <div>                    
+                    ${taskList[i].taskContent}
+                </div>
+                <div>
+                    <button onclick="toggleComplete('${taskList[i].id}')" class="btn btn-light"><i class="fa-solid fa-pause"></i></button>
+                    <button onclick="deleteTask('${taskList[i].id}')" id="delete-button" class="btn btn-light"><i class="fa-solid fa-stop"></i></button>
+                </div>
+            </div>
+            `
+        }
+    }
+    document.getElementById("task-board").innerHTML = resultHTML
+
+    let percent = taskList.length == 0 ? 0 : completeCount / taskList.length * 100
+    progressBar.style.width = `${percent}%`
+    percentInfo.textContent = `${percent}%`  
+}
+
+function toggleComplete(id){
+    for (let i = 0; i < taskList.length; i++){
+        if (taskList[i].id == id){
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+
+    render(taskStatus)
+}
+
+function deleteTask(id){
+    for (let i = 0; i < taskList.length; i++){
+        if (taskList[i].id == id){
+            taskList.splice(i, 1)
+            break;
+        }
+    }
+
+    render(taskStatus)
+}
+
+function showAllTasks(){
+    taskStatus = "ALL"
+
+    underLine.style.left = "0px"
+    render(taskStatus)
+}
+
+function showStartTasks(){
+    taskStatus = "START"
+
+    underLine.style.left = "75px"
+    render(taskStatus)
+}
+
+function showEndTasks(){
+    taskStatus = "END"
+
+    underLine.style.left = "150px"
+    render(taskStatus)
+}
